c1a19fed9e387581a54022fe55f0407c0221bff4
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-11-11 19:43:25 +0300

    Described the basic components + routes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { router } from './router/index'
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from './app/App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
---
+++ src/router/routes.ts
@@ -0,0 +1,20 @@
+import { RouteRecordRaw } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import BlogPage from '../views/BlogPage.vue'
+import ContactsPage from '../views/ContactsPage.vue'
+import FreePage from '../views/FreePage.vue'
+import ReviewsPage from '../views/ReviewsPage.vue'
+import SeminarsPage from '../views/SeminarsPage.vue'
+import ServicesPage from '../views/ServicesPage.vue'
+import TrainingsPage from '../views/TrainingsPage.vue'
+
+
+export const routes: RouteRecordRaw[] = [
+  { path: '', component: HomePage, name: 'home' },
+  { path: '/blog', component: BlogPage, name: 'blog' },
+  { path: '/contacts', component: ContactsPage, name: 'contacts' },
+  { path: '/free', component: FreePage, name: 'free' },
+  { path: '/reviews', component: ReviewsPage, name: 'reviews' },
+  { path: '/services', component: ServicesPage, name: 'services' },
+  { path: '/trainings', component: TrainingsPage, name: 'trainings' },
+]
