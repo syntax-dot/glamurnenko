20489c0f256e70b24edf1d4102c43fca4413966a
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-11-11 20:02:43 +0300

    add devDependencies for eslint

--- src/components/HeaderMenu/HeaderMenu.vue
+++ src/components/HeaderMenu/HeaderMenu.vue
@@ -1,11 +1,23 @@
 <template>
   <div :class="$style.root">
-  *
+  <div :class="$style.left">
+    <NavigationLink></NavigationLink>
+  </div>
+
+<div :class="$style.right">
+  <SocialLinks>
+  </SocialLinks>
+  <ContactsLinks></ContactsLinks>
+</div>
+
   </div>
 
 </template>
 
 <script lang="ts" setup>
+import { ContactsLinks } from '../ContactsLinks'
+import { NavigationLink } from '../NavigationLink'
+import { SocialLinks } from '../SocialLinks'
 
 </script>
 
